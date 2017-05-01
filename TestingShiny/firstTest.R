470f5d373f08d61129958940228263d348db63f8
Daniel Dinsdale <danieldinsdale:hotmail.co.uk>
2017-05-01 16:58:02 -0700

    completed lesson 2

---
+++ TestingShiny/firstTest.R
@@ -0,0 +1,7 @@
+library(shiny)
+#runExample("01_hello")
+setwd("C:/Users/Daniel/OneDrive/My Documents/UBC/ShinyCricket/TestingShiny")
+# run app normally
+runApp("firstApp")
+# run app with code visible
+runApp("firstApp", display.mode = "showcase")
