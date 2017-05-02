92b154fa887558cce4827dc3e56820868f0eef0d
Daniel Dinsdale <danieldinsdale:hotmail.co.uk>
2017-05-02 16:01:36 -0700

    completed lesson 5

--- TestingShiny/firstTest.R
+++ TestingShiny/firstTest.R
@@ -5,3 +5,11 @@ setwd("C:/Users/Daniel/OneDrive/My Documents/UBC/ShinyCricket/TestingShiny")
 runApp("firstApp")
 # run app with code visible
 runApp("firstApp", display.mode = "showcase")
+#
+# lesson 5
+
+library(maps)
+library(mapproj)
+source("census-app/helpers.R")
+counties <- readRDS("census-app/data/counties.rds")
+percent_map(counties$white, "darkgreen", "% White")
