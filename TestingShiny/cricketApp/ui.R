93304c3680bab0dde3ab6c56c405045ba4d5a1a1
Daniel Dinsdale <danieldinsdale:hotmail.co.uk>
2017-08-02 16:04:08 -0700

    updated layout and added special thanks page

--- TestingShiny/cricketApp/ui.R
+++ TestingShiny/cricketApp/ui.R
@@ -5,9 +5,11 @@ dashboardPage(
   dashboardHeader(title = div("IPL Statistics")),  
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Team Analysis", tabName = "teamAnalysis", icon = icon("th")),
-      menuItem("Bowling Analysis", tabName = "bowlingAnalysis", icon = icon("th")),
-      menuItem("Batting Analysis", tabName = "batsmanAnalysis", icon = icon("th"))
+      menuItem("Team Analysis", tabName = "teamAnalysis", icon = icon("bar-chart")),
+      menuItem("Bowling Analysis", tabName = "bowlingAnalysis", icon = icon("bar-chart")),
+      menuItem("Batting Analysis", tabName = "batsmanAnalysis", icon = icon("bar-chart")),
+      menuItem("Special Thanks", tabName = "specialThanks", icon = icon("bullhorn"))
+      
     )
   ),
   dashboardBody(
@@ -45,62 +47,63 @@ dashboardPage(
               )
       ),
       tabItem(tabName = "bowlingAnalysis",
+              titlePanel(title=div(img(src="IPLLogo.jpg", width=100))),
               fluidRow(
-                column(4,
+                column(4,wellPanel( 
                        selectInput("bowlingTeam", 
                                    label = "Select an IPL team to analyse",
                                    choices = c("Deccan Chargers", "Delhi Daredevils", "Kings XI Punjab", "Kochi Tuskers Kerala", "Kolkata Knight Riders", 
                                                "Rajasthan Royals", "Royal Challengers Bangalore", "Sunrisers Hyderabad", "Chennai Super Kings", 
                                                "Mumbai Indians", "Pune Warriors"),
-                                   selected = "Mumbai Indians")
-                )
-              ),
+                                   selected = "Mumbai Indians"),
+                       uiOutput("bowlerSelection"),
+                       selectInput("bowlerInfo", 
+                                   label = "Choose an analysis type",
+                                   choices = c("Economy Rate", "Wickets vs Opposition", "Venue Record"),
+                                   selected = "Economy Rate")
+                ),
+                column(4,
+                       htmlOutput("bowlerPicture")
+              )),
               column(4,
-                     uiOutput("bowlerSelection"),
-                     htmlOutput("bowlerPicture")
-              ),
-              column(4,
-                     selectInput("bowlerInfo", 
-                                 label = "Choose an analysis type",
-                                 choices = c("Economy Rate", "Wickets vs Opposition", "Venue Record"),
-                                 selected = "Economy Rate"),
                      mainPanel(plotOutput("bowlerPlot", width = 600))
           )
-      ),
+      )),
       tabItem(tabName = "batsmanAnalysis",
+              titlePanel(title=div(img(src="IPLLogo.jpg", width=100))),
               fluidRow(
-                column(4,
+                column(4, wellPanel(
                        selectInput("batsmanTeam", 
                                    label = "Select an IPL team to analyse",
                                    choices = c("Deccan Chargers", "Delhi Daredevils", "Kings XI Punjab", "Kochi Tuskers Kerala", "Kolkata Knight Riders", 
                                                "Rajasthan Royals", "Royal Challengers Bangalore", "Sunrisers Hyderabad", "Chennai Super Kings", 
                                                "Mumbai Indians", "Pune Warriors"),
-                                   selected = "Delhi Daredevils")
-                )
-              ),
-              column(4,
-                     uiOutput("batsmanSelection"),
-                     htmlOutput("batsmanPicture")
-              ),
-              column(4,
-                     selectInput("batsmanInfo", 
+                                   selected = "Delhi Daredevils"),
+                       uiOutput("batsmanSelection"),
+                       selectInput("batsmanInfo", 
                                    label = "Choose an analysis type",
                                    choices = c("Dismissal Type", "Runs vs Deliveries", "Opposition Record", "Venue Record"),
-                                 selected = "Dismissal Type"),
+                                   selected = "Dismissal Type")
+              ),
+              column(4,
+                     htmlOutput("batsmanPicture")
+              )),
+              column(4,
                      mainPanel(plotOutput("batsmanPlot", width = 600))
               )
+         )
+      ),
+      tabItem(tabName = "specialThanks",
+              titlePanel(title=div(img(src="IPLLogo.jpg", width=100))),
+              fluidRow(
+                column(7, 
+                  h1("A special thanks to:"),
+                  p("The R package", span(a("yorkr", href="https://cran.r-project.org/web/packages/yorkr/README.html")), "for data extraction and plotting code."),                
+                  p(span(a("Cricinfo", href="http://www.espncricinfo.com")), "for player portraits."),
+                  p(span(a("Cricsheet", href="http://cricsheet.org/"), "for the game data."))                
+                  )
+              )
       )
-      # ),
-      # tabItem(tabName = "players",
-      #         fluidRow(
-      #           column(4,
-      #                  #mainPanel(plotOutput("plot", width = 600)),
-      #                  #mainPanel(plotOutput("teamLogo", width = 20)),
-      #                  # textInput("cricketPortrait", "Random Cricket Player", value = "Tim Bresnan", width = NULL, placeholder = NULL),
-      #                  # htmlOutput("picture")
-      #           )
-      #         )
-      # )
     )
   )
 )
